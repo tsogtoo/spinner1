6df2cf353afe446a3ee18639421654ef9b519689
malaga <malaga@glass>
2013-05-14 14:12:21 +0800

    exmp2

--- src/com/example/spinner/MainActivity.java
+++ src/com/example/spinner/MainActivity.java
@@ -1,49 +1,115 @@
 package com.example.spinner;
 
+
 import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.FilterQueryProvider;
 import android.widget.ListView;
-import android.widget.Spinner;
-import android.widget.TextView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
-public class MainActivity extends Activity implements OnItemSelectedListener {
-    /** Called when the activity is first created. */
-	private TextView userSelection;
-	
-	private static final String[] items={"Android","Bluetooth","Chrome","Docs","Email",
-        "Facebook","Google","Hungary","Iphone","Korea","Machintosh",
-        "Nokia","Orkut","Picasa","Singapore","Turkey","Windows","Youtube"};
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        userSelection=(TextView)findViewById(R.id.user_selection);
-        
-        Spinner my_spin=(Spinner)findViewById(R.id.my_spinner);
-        
-        my_spin.setOnItemSelectedListener(this);
-        
-        ArrayAdapter aa=new ArrayAdapter(this, android.R.layout.simple_spinner_item,items);
-        
-        aa.setDropDownViewResource(android.R.layout.simple_spinner_item);
-        
-        my_spin.setAdapter(aa);
-    }
-	@Override
-	public void onItemSelected(AdapterView<?> arg0, View arg1, int pos,
-			long arg3) {
-		userSelection.setText(items[pos]);
-	}
-	@Override
-	public void onNothingSelected(AdapterView<?> arg0) {
-		// TODO Auto-generated method stub
-		userSelection.setText("");
-	}
+public class MainActivity extends Activity {
+
+ private CountriesDbAdapter dbHelper;
+ private SimpleCursorAdapter dataAdapter;
+
+ @Override
+ public void onCreate(Bundle savedInstanceState) {
+  super.onCreate(savedInstanceState);
+  setContentView(R.layout.activity_main);
+
+  dbHelper = new CountriesDbAdapter(this);
+  dbHelper.open();
+
+  //Clean all data
+  dbHelper.deleteAllCountries();
+  //Add some data
+  dbHelper.insertSomeCountries();
+
+  //Generate ListView from SQLite Database
+  displayListView();
+
+ }
+
+ private void displayListView() {
+
+
+  Cursor cursor = dbHelper.fetchAllCountries();
+
+  // The desired columns to be bound
+  String[] columns = new String[] {
+    CountriesDbAdapter.KEY_CODE,
+    CountriesDbAdapter.KEY_NAME,
+    CountriesDbAdapter.KEY_CONTINENT,
+    CountriesDbAdapter.KEY_REGION
+  };
+
+  // the XML defined views which the data will be bound to
+  int[] to = new int[] { 
+    R.id.code,
+    R.id.name,
+    R.id.continent,
+    R.id.region,
+  };
+
+  // create the adapter using the cursor pointing to the desired data 
+  //as well as the layout information
+  dataAdapter = new SimpleCursorAdapter(
+    this, R.layout.country_info, 
+    cursor, 
+    columns, 
+    to,
+    0);
+
+  ListView listView = (ListView) findViewById(R.id.listView1);
+  // Assign adapter to ListView
+  listView.setAdapter(dataAdapter);
+
+
+  listView.setOnItemClickListener(new OnItemClickListener() {
+   @Override
+   public void onItemClick(AdapterView<?> listView, View view, 
+     int position, long id) {
+   // Get the cursor, positioned to the corresponding row in the result set
+   Cursor cursor = (Cursor) listView.getItemAtPosition(position);
+
+   // Get the state's capital from this row in the database.
+   String countryCode = 
+    cursor.getString(cursor.getColumnIndexOrThrow("code"));
+   Toast.makeText(getApplicationContext(),
+     countryCode, Toast.LENGTH_SHORT).show();
+
+   }
+  });
+
+  EditText myFilter = (EditText) findViewById(R.id.myFilter);
+  myFilter.addTextChangedListener(new TextWatcher() {
+
+   public void afterTextChanged(Editable s) {
+   }
+
+   public void beforeTextChanged(CharSequence s, int start, 
+     int count, int after) {
+   }
+
+   public void onTextChanged(CharSequence s, int start, 
+     int before, int count) {
+    dataAdapter.getFilter().filter(s.toString());
+   }
+  });
+  
+  dataAdapter.setFilterQueryProvider(new FilterQueryProvider() {
+         public Cursor runQuery(CharSequence constraint) {
+             return dbHelper.fetchCountriesByName(constraint.toString());
+         }
+     });
+
+ }
 }
-
